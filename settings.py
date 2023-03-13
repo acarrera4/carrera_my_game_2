901e472e495d05e8b463b3dfc6516dbe38052080
acarrera4 <aleccarrera4@gmail.com>
2023-03-13 11:26:29 -0700

    testing commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+#file created by: Alec Carrera
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+
+# player attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50, 50, 255)
+
+RUNNING = True
