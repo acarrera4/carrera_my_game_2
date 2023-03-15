0af25fbf759ee6f0a3abf6e21e5b697d311a9d92
acarrera4 <aleccarrera4@gmail.com>
2023-03-15 08:50:19 -0700

    test

--- settings.py
+++ settings.py
@@ -6,13 +6,16 @@ HEIGHT = 600
 
 # game settings:
 FPS = 30
+RUNNING = True
 
 # player attributes
 PLAYER_ACC  = 2
+MOB_ACC = 2
+
 PLAYER_FRICTION = -0.12
+MOB_FRICTION = -0.12
 
 # colors
 BLACK = (0,0,0)
 BLUE = (50, 50, 255)
-
-RUNNING = True
+RED = (255, 50, 50)
